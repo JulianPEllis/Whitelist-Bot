a0d38315bd83246bcb87c7a4223199ff838fcadc
Julian Ellis <45700090+JulianPEllis@users.noreply.github.com>
2019-05-06 20:42:52 -0400

    Added Toggle Command

--- bot.py
+++ bot.py
@@ -8,6 +8,8 @@ bot = commands.Bot(command_prefix = '-')
 
 whitelistedUsers = ['Mashhhyyy#7521', 'Example#1234', 'WhitelistedUser#54321']
 
+status = true
+
 TOKEN = 'BOT_TOKEN_HERE'
 
 @bot.event
@@ -17,8 +19,20 @@ async def on_ready():
 
 @bot.event
 async def on_member_join(member):
-    if str(member) not in whitelistedUsers:
-        await bot.kick(str(member))
-
+    if status == true:
+        if str(member) not in whitelistedUsers:
+            await bot.kick(str(member))
+    elif status == false:
+        print('{} joined!'.format(member))
+        
+@bot.command(pass_context = True)
+async def toggle(ctx):
+    if status == true:
+        status = false
+        await bot.say('Whitelist Toggled Off')
+    elif status == false:
+        status = true
+        await bot.say('Whitelist Toggled On')
+        
 
 bot.run(TOKEN)
